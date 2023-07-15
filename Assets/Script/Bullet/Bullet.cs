8a02d97b408ac7bca98b27e33b11ee1e17c6fde3
준원 <swjwpower@gmail.com>
2023-07-15 15:28:02 +0900

    불렛 인터페이스 및 상속화

--- Assets/Script/Bullet/Bullet.cs
+++ Assets/Script/Bullet/Bullet.cs
@@ -3,39 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public interface IBulletStat
+{
+    public BulletStat ReturnBulletStat();
+}
+
+public class Bullet:MonoBehaviour, IBulletStat
+{
+    public Vector2 direction;
+    private Rigidbody2D rigidbody=null;
+    public BulletStat myStat = new BulletStat();
+    private void Start()//인스턴스화 하면 캐논에서 받아온 direction으로 발사된다.
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = myStat.direction.normalized * myStat.speed;
+    }
+
+    public BulletStat ReturnBulletStat()
+    {
+        return myStat;
+    }
+}
+
 [Serializable]
-public struct BulletStat
+public class BulletStat
 {
     public Vector2 direction;
     public int power;
     public float speed;
 }
-public enum TYPE
-{
-    Normal, Fire, Bird, Curse
-}
-
-public class Bullet:MonoBehaviour
-{
-    public Vector2 direction;
-    private Rigidbody2D rigidbody=null;
-    public static Bullet instance;
-    public BulletStat myStat;
-    public TYPE type = TYPE.Normal;
-    SpriteRenderer _spriteRenderer = null;
-    private void Awake()
-    {
-        instance = this;
-    }
-    private void Start()//인스턴스화 하면 캐논에서 받아온 direction으로 발사된다.
-    {
-        rigidbody = GetComponent<Rigidbody2D>();
-        //myStat.power= BulletShoot.instance.bulletPower;
-        //myStat.speed = BulletShoot.instance.bulletSpeed;    //필드 버튼(normal, fire, curse)으로 결정되는 공격력과 속도를 전달
-        Debug.Log(myStat.speed);
-        rigidbody.velocity = direction.normalized * myStat.speed;
-    }
-  
-
-}
-
