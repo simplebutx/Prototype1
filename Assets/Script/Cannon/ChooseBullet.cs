0a0d2443799fddd9a02054e5863b491f2db9c322
htmluiv <73861559+htmluiv@users.noreply.github.com>
2023-07-15 15:54:29 +0900

    0715
    edit

--- Assets/Script/Cannon/ChooseBullet.cs
+++ Assets/Script/Cannon/ChooseBullet.cs
@@ -22,7 +22,7 @@ public class ChooseBullet : MonoBehaviour
 
 
 
-    public void Start()
+    public void Start() //각 버튼에 해당 총알 가져오는 이벤트 할당
     {
         Btn_Bullet1.onClick.AddListener(Bullet1Clicked);
         Btn_Bullet2.onClick.AddListener(Bullet2Clicked);
