533bc92b7ba72dfe37143cb21fdc8dd9ad0a5518
htmluiv <73861559+htmluiv@users.noreply.github.com>
2023-07-22 14:35:30 +0900

    0722
    Drag UI

--- Assets/Script/SettingComplete.cs
+++ Assets/Script/SettingComplete.cs
@@ -6,11 +6,14 @@ public class SettingComplete : MonoBehaviour
 {
     public GameObject slot;
     public GameObject dragPoint;
+    public GameObject mainCamera;
 
     public void EndSetting()//세팅 끝나면 슬롯 스프라이트 비활성 및 드래그 포인트 활성
     {
         slot.SetActive(false);
         dragPoint.SetActive(true);
+        mainCamera.GetComponent<ScreenDrag>().enabled = false;
+
         DataController.instance.gameData.turn += 1;     //게임에 들어가면 턴이 증가
         Debug.Log($"{DataController.instance.gameData.turn}턴");
     }
