0a65e137bd33f47f79bdc2928880ab5ad67343c0
Hszrin <swjwpower@gmail.com>
2023-07-31 01:02:12 +0900

    7-31
    누락요소 추가

--- Assets/Script/Monster/Monster.cs
+++ Assets/Script/Monster/Monster.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public interface IMonsterCollision//총알과 충돌시 hp업데이트 인터페이스
 {
@@ -18,14 +19,18 @@ public class Monster :MonoBehaviour, IMonsterStat, IMonsterCollision,IPointerCli
     public GameObject xButton;
     public bool cursed = false;
     public float percentage = 0.3f;//나중에 1성~3성에 따라 외부에서 가져올 값
+    private GameObject damageTextPrefab;
+    private GameObject damageTextCopy;
     private void Start()
     {
         onDestroy.AddListener(DestroySelf);
+        damageTextPrefab = Resources.Load("DamageText") as GameObject;
     }
 
     public bool UpdateHp(float damage)
     {
         monsterStat.hp -= damage;
+        HpMinusTextFloating(damage);
 
         if (monsterStat.hp <= 0)
         {
@@ -34,6 +39,12 @@ public class Monster :MonoBehaviour, IMonsterStat, IMonsterCollision,IPointerCli
         }
         hpSlider.value = monsterStat.hp / monsterStat.maxHp ;
         return false;
+    }
+    public void HpMinusTextFloating(float val)
+    {
+        damageTextCopy = Instantiate(damageTextPrefab);
+        damageTextCopy.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = val.ToString();
+        damageTextCopy.transform.localPosition = transform.position;
     }
 
     public void DestroySelf()
